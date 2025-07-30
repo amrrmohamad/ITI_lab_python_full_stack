7fc7206dfbcf420eab83768be7c2a79b79500ed8
amrrmohamad <amr.r.mohamad55@gmail.com>
2025-07-30 07:21:08 +0300

    iti

--- week 1/Amr_Mohamad_Bakr_lab_4.py
+++ week 1/Amr_Mohamad_Bakr_lab_4.py
@@ -1,33 +1,32 @@
-
 """
 - this sys is responsible for managing cars and motorcycle data
-- sell for new and used cars and show 
-- calculate the sales for the agency , comparing cars [power] and for used cars i can show how much km is move 
+- sell for new and used cars and show
+- calculate the sales for the agency , comparing cars [power] and for used cars i can show how much km is move
 
-- renting cars for customers 
+- renting cars for customers
 
-- buying options 
+- buying options
 """
 
+
 class Race_Car:
-	def __init__(self,name,color,model,staus,power,price,day_rent):
-		self.name = name
-		self.color = color
-		self.model = model
-		self.staus = staus
-		self.day_rent = day_rent
-		self.power = int(power)
-		self.price = int(price)
-		if self.price < 10000:
-			print(f"sorry your price '{self.price}$' in not enough")
+    def __init__(self, name, color, model, staus, power, price, day_rent):
+        self.name = name
+        self.color = color
+        self.model = model
+        self.staus = staus
+        self.day_rent = day_rent
+        self.power = int(power)
+        self.price = int(price)
+        if self.price < 10000:
+            print(f"sorry your price '{self.price}$' in not enough")
 
-	def __str__(self):
-			return self.name
-
+    def __str__(self):
+        return self.name
 
 
 class Classic_Car:
-    def __init__(self,name,color,model,staus,power,price, day_rent):
+    def __init__(self, name, color, model, staus, power, price, day_rent):
         self.name = name
         self.color = color
         self.model = model
@@ -39,41 +38,47 @@ class Classic_Car:
     def __str__(self):
         return self.name
 
-#===============================================================================
+
+# ===============================================================================
+
 
 class Motorcycle:
-    def __init__(self,name,color,model,staus,power,price):
+    def __init__(self, name, color, model, staus, power, price):
         self.name = name
         self.color = color
         self.model = model
         self.staus = staus
-        
+
         self.power = int(power)
         self.price = int(price)
 
     def __str__(self):
         return self.name
 
-#===============================================================================
 
-class Employee():
-    def __init__(self, name, salary ,commision,role):
+# ===============================================================================
+
+
+class Employee:
+    def __init__(self, name, salary, commision, role):
         self.name = name
         self.salary = int(salary)
         self.commisison = commision
         self.role = role
 
     def check(self):
-        return self.salary + self.commisison 
+        return self.salary + self.commisison
 
     def __str__(self):
         return self.name
 
-#===============================================================================
+
+# ===============================================================================
+
 
 class Customer(Classic_Car, Race_Car):
-    
-    def __init__(self, name,deposit,status):
+
+    def __init__(self, name, deposit, status):
         self.name = name
         self.deposit = int(deposit)
         self.status = status
@@ -81,7 +86,9 @@ class Customer(Classic_Car, Race_Car):
     def __str__(self):
         return self.name
 
-#===============================================================================
+
+# ===============================================================================
+
 
 class Agancy:
 
@@ -89,40 +96,44 @@ class Agancy:
 
     def __init__(self):
         self.cars = []
-        self.motorcycles=[]
+        self.motorcycles = []
         self.selled_cars = []
         self.employees = []
 
     def insert(self, item):
-        if isinstance(item,Race_Car) or isinstance(item, Classic_Car):
+        if isinstance(item, Race_Car) or isinstance(item, Classic_Car):
             self.cars.append(item)
             print(f"you add this car to your agancy {item}")
         else:
             self.motorcycles.append(item)
             print(f"you add this motorcycle to your agancy {item}")
 
-    def get_all(self,item):
-        if item == 'cars':
+    def get_all(self, item):
+        if item == "cars":
             for car in self.cars:
                 print(str(car))
         else:
             for mororcycle in self.motorcycles:
                 print(str(mororcycle))
 
-    def sell_this(self,item,customer,employee):
-        # add the price for the sales 
+    def sell_this(self, item, customer, employee):
+        # add the price for the sales
         self.selled_cars.append(item)
         self.sales += item.price
         self.cars.remove(item)
         # sale=Sell(item,customer)
-        commisison = item.price*0.025
+        commisison = item.price * 0.025
         employee.commisison += commisison
         self.sales -= commisison
         self.employees.append(employee)
-        print(f"this sell done by {employee.name} for {item} selled to {customer.name} ")
+        print(
+            f"this sell done by {employee.name} for {item} selled to {customer.name} "
+        )
 
     def compare_power(self, car1, car2):
-        if isinstance(car1, (Classic_Car, Race_Car)) and isinstance(car2, (Classic_Car, Race_Car)):
+        if isinstance(car1, (Classic_Car, Race_Car)) and isinstance(
+            car2, (Classic_Car, Race_Car)
+        ):
             if car1.power > car2.power:
                 return f"{car1.name} : ({car1.power} PH) is more powerful than {car2.name} : ({car2.power} PH)."
             elif car1.power < car2.power:
@@ -136,53 +147,52 @@ class Agancy:
         car.is_rented = False
         if car.is_rented:
             return f"Sorry, {car.name} is rented."
-        
+
         rent_cost = rent_days * car.day_rent
-        
+
         if customer.deposit < rent_cost:
             return f"not enough deposit for renting {car.name} => Required: ${rent_cost} : Available: ${customer.deposit}"
 
         car.is_rented = True
         customer.deposit -= rent_cost
         self.sales += rent_cost
-        
-        return f"{car.name} rented to {customer.name} for {rent_days} days. Total cost: ${rent_cost}"
 
+        return f"{car.name} rented to {customer.name} for {rent_days} days. Total cost: ${rent_cost}"
 
     def __str__(self):
         return "i'm agancy for cars and motor cycles"
 
 
-#================================================================================
+# ================================================================================
 
-car1= Classic_Car('toyota','black','crola','new',200,5000, 100)
-car2= Race_Car('Ferrari','red','F8 spider','new',750,1000, 200)
-motorcycle1 = Motorcycle('Harley-Davidson', 'Black', 'Iron 883', 'New', 100, 9000)
+car1 = Classic_Car("toyota", "black", "crola", "new", 200, 5000, 100)
+car2 = Race_Car("Ferrari", "red", "F8 spider", "new", 750, 1000, 200)
+motorcycle1 = Motorcycle("Harley-Davidson", "Black", "Iron 883", "New", 100, 9000)
 
-agancy=Agancy()
+agancy = Agancy()
 agancy.insert(car1)
 agancy.insert(car2)
 agancy.insert(motorcycle1)
 
 
-customer1=Customer('hamada1',20000,'cash')
-customer2=Customer('mohamad', 5000, 'cash')
-empoloyee=Employee('hamada_seller',500,0,'seller')
+customer1 = Customer("hamada1", 20000, "cash")
+customer2 = Customer("mohamad", 5000, "cash")
+empoloyee = Employee("hamada_seller", 500, 0, "seller")
 
-agancy.sell_this(car1,customer1,empoloyee)
-agancy.sell_this(car2,customer2,empoloyee)
+agancy.sell_this(car1, customer1, empoloyee)
+agancy.sell_this(car2, customer2, empoloyee)
 
-print("="*50)
+print("=" * 50)
 compare1 = agancy.compare_power(car1, car2)
 compare2 = agancy.compare_power(car1, motorcycle1)
 compare3 = agancy.compare_power(car2, motorcycle1)
 print(compare1, compare2, compare3, sep="\n\n")
-print("="*50)
+print("=" * 50)
 
 rent = agancy.rent_car(car1, customer2, 10)
-print(rent ,sep="\n")
-print('================= sales ==================')
+print(rent, sep="\n")
+print("================= sales ==================")
 print(agancy.sales)
 print(empoloyee.check())
-print('==========================================')
-print(agancy.get_all('cars'))
+print("==========================================")
+print(agancy.get_all("cars"))
--- week 1/Amr_Mohamad_Bakr_lab_5.py
+++ week 1/Amr_Mohamad_Bakr_lab_5.py
@@ -1,91 +1,91 @@
-from flask import Flask, jsonify,request
+from flask import Flask, jsonify, request
 import sqlite3
 
-database='test.db'
+database = "test.db"
+
 
 def connect_db():
-    connetion=sqlite3.connect(database)
-    connetion.row_factory = sqlite3.Row 
+    connetion = sqlite3.connect(database)
+    connetion.row_factory = sqlite3.Row
     return connetion
-    
-# start the database and create the tables in db 
+
+
+# start the database and create the tables in db
 def db_creation():
     conn = connect_db()
-    conn.execute("""
+    conn.execute(
+        """
                 CREATE TABLE IF NOT EXISTS users(
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                     name VARCHAR(255) NOT NULL,
                     age INTEGER ,
                     email VARCHAR(255) NOT NULL
                 );
-                """)
+                """
+    )
     conn.close()
-    
+
+
 db_creation()
 
 app = Flask(__name__)
 
 # request -> process[get data,formation ,call ...] -> response
 
-# crud 
-# ->      
 
-# 
-
-@app.route(
-    '/users/',methods=['GET']
-)
+@app.route("/users/", methods=["GET"])
 def get_users():
-    conn=connect_db()
-    data=conn.execute("SELECT * FROM users").fetchall()
+    conn = connect_db()
+    data = conn.execute("SELECT * FROM users").fetchall()
     users = [dict(row) for row in data]
     conn.close()
-    return jsonify(users) 
+    return jsonify(users)
 
 
-@app.route("/users/",methods=['POST'])
+@app.route("/users/", methods=["POST"])
 def insert_user():
     data = request.get_json()
-    name=data.get('name')
-    email = data.get('email')
-    if name  and email :
-        conn=connect_db()
-        conn.execute("INSERT INTO users (name, email) VALUES (?,?)",(name,email))
+    name = data.get("name")
+    email = data.get("email")
+    if name and email:
+        conn = connect_db()
+        conn.execute("INSERT INTO users (name, email) VALUES (?,?)", (name, email))
         conn.commit()
         conn.close()
-        return jsonify({"message":"user inserted successfully"})
+        return jsonify({"message": "user inserted successfully"})
 
 
 # Update an existing To-Do item
-@app.route("/users/", methods=['PUT'])
+@app.route("/users/", methods=["PUT"])
 def update_todo(id):
     data = request.get_json()
-    title = data.get('title')
-    description = data.get('description')
-    status = data.get('status')
-    
+    title = data.get("title")
+    description = data.get("description")
+    status = data.get("status")
+
     conn = connect_db()
     users = conn.execute("SELECT * FROM users WHERE id = ?", (id,)).fetchone()
-    
+
     if users:
-        updated_title = title if title else users['title']
-        updated_description = description if description else users['description']
-        updated_status = status if status else users['status']
-        
-        conn.execute("UPDATE users SET title = ?, description = ?, status = ? WHERE id = ?",
-                     (updated_title, updated_description, updated_status, id))
+        updated_title = title if title else users["title"]
+        updated_description = description if description else users["description"]
+        updated_status = status if status else users["status"]
+
+        conn.execute(
+            "UPDATE users SET title = ?, description = ?, status = ? WHERE id = ?",
+            (updated_title, updated_description, updated_status, id),
+        )
         conn.commit()
         conn.close()
         return jsonify({"message": "To-Do item updated successfully"})
 
 
-
 # Delete a To-Do item
-@app.route("/users/", methods=['DELETE'])
+@app.route("/users/", methods=["DELETE"])
 def delete_todo(id):
     conn = connect_db()
     users = conn.execute("SELECT * FROM users WHERE id = ?", (id,)).fetchone()
-    
+
     if users:
         conn.execute("DELETE FROM users WHERE id = ?", (id,))
         conn.commit()
@@ -93,4 +93,4 @@ def delete_todo(id):
         return jsonify({"message": "To-Do item deleted successfully"})
 
 
-app.run(host='0.0.0.0', port=5000)
+app.run(host="0.0.0.0", port=5000)
